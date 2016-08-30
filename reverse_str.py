08bb414130cea57ea05efd4d46063da8ab0e5929
eric.shih <force416@gmail.com>
2016-08-30 12:28:04 +0800

    add reverse string pratice code

--- reverse_str.py
+++ reverse_str.py
@@ -1,3 +1,9 @@
+"""
+https://leetcode.com/problems/reverse-string/
+Write a function that takes a string as input and returns the string reversed.
+Example:
+Given s = "hello", return "olleh".
+"""
 class Solution(object):
     def reverseString(self, s):
         """
@@ -5,3 +11,8 @@ class Solution(object):
         :rtype: str
         """
         return s[::-1]
+
+#測試程式        
+if __name__ == '__main__':
+    s = Solution()
+    print(s.reverseString('hello!'))
